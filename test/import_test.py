14d52f942c32ccc8dc108b1db1cb031c237dc062
wsprague-nu <williamsprague2025@u.northwestern.edu>
2022-07-19 12:48:00 -0500

    fixed formatting

--- test/import_test.py
+++ test/import_test.py
@@ -6,6 +6,6 @@ import pickaxe_generic
 pkg_name = pickaxe_generic.__name__
 pkg_path = pickaxe_generic.__path__
 
-for _, modname, _ in iter_modules(pkg_path, f"{pkg_name}."): # type: ignore
+for _, modname, _ in iter_modules(pkg_path, f"{pkg_name}."):  # type: ignore
     module = import_module(modname, "pickaxe_generic")
     print(f"Imported {module}")
