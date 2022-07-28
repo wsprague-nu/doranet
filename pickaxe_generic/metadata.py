9354f0aa6150cca5f43ba5e9c85793eb9a0cd9d0
wsprague-nu <williamsprague2025@u.northwestern.edu>
2022-07-28 15:39:37 -0500

    added some simple classes to metadata.py

--- pickaxe_generic/metadata.py
+++ pickaxe_generic/metadata.py
@@ -2,10 +2,10 @@ from abc import ABC, abstractmethod
 from collections.abc import Collection, Mapping
 from functools import reduce
 from operator import add, or_
-from typing import Protocol, final
+from typing import Optional, Protocol, final
 
 from pickaxe_generic.datatypes import MetaKeyPacket
-from pickaxe_generic.network import ReactionExplicit
+from pickaxe_generic.network import ChemNetwork, ReactionExplicit
 
 
 class MetaSink(Protocol):
@@ -32,7 +32,6 @@ class LocalPropertyCalc(MetaSink, ABC):
 @final
 class LocalPropertyCalcMultiple(LocalPropertyCalc):
     __slots__ = "_composed_properties"
-
     _composed_properties: tuple[LocalPropertyCalc, ...]
 
     def __init__(self, prop_list: Collection[LocalPropertyCalc]) -> None:
@@ -42,6 +41,7 @@ class LocalPropertyCalcMultiple(LocalPropertyCalc):
                 props.extend(prop._composed_properties)
             else:
                 props.append(prop)
+        self._composed_properties = tuple(props)
 
     def __call__(self, reaction: ReactionExplicit) -> dict:
         return reduce(
@@ -54,3 +54,57 @@ class LocalPropertyCalcMultiple(LocalPropertyCalc):
         return reduce(
             add, (prop.meta_required for prop in self._composed_properties)
         )
+
+
+class MetaUpdate(ABC):
+    @abstractmethod
+    def __call__(
+        self, reaction: ReactionExplicit, network: ChemNetwork
+    ) -> Optional[ReactionExplicit]:
+        ...
+
+    @final
+    def __add__(self, other: "MetaUpdate") -> "MetaUpdate":
+        return MetaUpdateMultiple((self, other))
+
+
+@final
+class MetaUpdateMultiple(MetaUpdate):
+    __slots__ = "_composed_updates"
+    _composed_updates: tuple[MetaUpdate, ...]
+
+    def __init__(self, update_list: Collection[MetaUpdate]) -> None:
+        updates: list[MetaUpdate] = []
+        for update in update_list:
+            if isinstance(update, MetaUpdateMultiple):
+                updates.extend(update._composed_updates)
+            else:
+                updates.append(update)
+        self._composed_updates = tuple(updates)
+
+    def __call__(
+        self, reaction: ReactionExplicit, network: ChemNetwork
+    ) -> ReactionExplicit:
+        cur_reaction = reaction
+        for update in self._composed_updates:
+            new_reaction = update(cur_reaction, network)
+            if new_reaction is not None:
+                cur_reaction = new_reaction
+        return cur_reaction
+
+
+@final
+class MetaUpdateDefault(MetaUpdate):
+    __slots__ = ()
+
+    def __call__(
+        self, reaction: ReactionExplicit, network: ChemNetwork
+    ) -> None:
+        op_meta = reaction.operator_meta
+        if op_meta is not None:
+            op_index = network.ops.i(reaction.operator.uid)
+            for key, value in op_meta:
+                network.op_meta(op_index, key, value)
+        react_meta = reaction.reactants_meta
+        if react_meta is not None:
+            ...
