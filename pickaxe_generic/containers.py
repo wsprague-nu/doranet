f4b304b82c95c785aad98d4c0ac67876b46d956f
wsprague-nu <williamsprague2025@u.northwestern.edu>
2022-09-19 09:54:04 -0500

    updated typing

--- pickaxe_generic/containers.py
+++ pickaxe_generic/containers.py
@@ -8,23 +8,16 @@ Classes:
       ObjectLibraryKeyVal*
 """
 
-from typing import (
-    Callable,
-    Dict,
-    Generator,
-    Generic,
-    Iterable,
-    Iterator,
-    Optional,
-    Union,
-    final,
-)
+import collections.abc
+import typing
 
 from . import interfaces
 
 
-@final
-class ObjectLibraryBasic(interfaces.ObjectLibrary, Generic[interfaces.T_data]):
+@typing.final
+class ObjectLibraryBasic(
+    interfaces.ObjectLibrary, typing.Generic[interfaces.T_data]
+):
     """
     Minimal class implementing the ObjectLibrary interface.
 
@@ -38,31 +31,38 @@ class ObjectLibraryBasic(interfaces.ObjectLibrary, Generic[interfaces.T_data]):
 
     __slots__ = ("_lookup",)
 
-    _lookup: Dict[interfaces.Identifier, interfaces.T_data]
+    _lookup: dict[interfaces.Identifier, interfaces.T_data]
 
     def __init__(
         self,
-        objects: Optional[
-            Union[Iterable[interfaces.T_data], interfaces.T_data]
+        objects: typing.Optional[
+            typing.Union[
+                collections.abc.Iterable[interfaces.T_data], interfaces.T_data
+            ]
         ] = None,
     ) -> None:
         self._lookup = {}
-        if isinstance(objects, Iterable):
+        if isinstance(objects, collections.abc.Iterable):
             for item in objects:
                 self._lookup[item.uid] = item
         elif objects is not None:
             self._lookup[objects.uid] = objects
 
     def add(
-        self, obj: Union[Iterable[interfaces.T_data], interfaces.T_data]
+        self,
+        obj: typing.Union[
+            collections.abc.Iterable[interfaces.T_data], interfaces.T_data
+        ],
     ) -> None:
-        if isinstance(obj, Iterable):
+        if isinstance(obj, collections.abc.Iterable):
             for item in obj:
                 self._lookup[item.uid] = item
         else:
             self._lookup[obj.uid] = obj
 
-    def ids(self) -> Generator[interfaces.Identifier, None, None]:
+    def ids(
+        self,
+    ) -> collections.abc.Generator[interfaces.Identifier, None, None]:
         return (key for key in self._lookup)
 
     def __contains__(self, item: interfaces.T_data) -> bool:
@@ -71,15 +71,17 @@ class ObjectLibraryBasic(interfaces.ObjectLibrary, Generic[interfaces.T_data]):
     def __getitem__(self, item: interfaces.Identifier) -> interfaces.T_data:
         return self._lookup[item]
 
-    def __iter__(self) -> Iterator[interfaces.T_data]:
+    def __iter__(self) -> collections.abc.Iterator[interfaces.T_data]:
         return iter(self._lookup.values())
 
     def __len__(self) -> int:
         return len(self._lookup)
 
 
-@final
-class ObjectLibraryKeyVal(interfaces.ObjectLibrary, Generic[interfaces.T_data]):
+@typing.final
+class ObjectLibraryKeyVal(
+    interfaces.ObjectLibrary, typing.Generic[interfaces.T_data]
+):
     """
     Minimal class implementing the ObjectLibrary interface.
 
@@ -99,35 +101,44 @@ class ObjectLibraryKeyVal(interfaces.ObjectLibrary, Generic[interfaces.T_data]):
         "_lookup",
     )
 
-    _lookup: Dict[interfaces.Identifier, bytes]
+    _lookup: dict[interfaces.Identifier, bytes]
 
     def __init__(
         self,
-        objects: Optional[
-            Union[Iterable[interfaces.T_data], interfaces.T_data]
+        objects: typing.Optional[
+            typing.Union[
+                collections.abc.Iterable[interfaces.T_data], interfaces.T_data
+            ]
         ] = None,
-        initializer: Optional[Callable[[bytes], interfaces.T_data]] = None,
+        initializer: typing.Optional[
+            collections.abc.Callable[[bytes], interfaces.T_data]
+        ] = None,
     ) -> None:
         if initializer is None:
             raise ValueError("initializer must be specified")
         self._lookup = {}
         self._initializer = initializer
-        if isinstance(objects, Iterable):
+        if isinstance(objects, collections.abc.Iterable):
             for item in objects:
                 self._lookup[item.uid] = item.blob
         elif objects is not None:
             self._lookup[objects.uid] = objects.blob
 
     def add(
-        self, obj: Union[Iterable[interfaces.T_data], interfaces.T_data]
+        self,
+        obj: typing.Union[
+            collections.abc.Iterable[interfaces.T_data], interfaces.T_data
+        ],
     ) -> None:
-        if isinstance(obj, Iterable):
+        if isinstance(obj, collections.abc.Iterable):
             for item in obj:
                 self._lookup[item.uid] = item.blob
         else:
             self._lookup[obj.uid] = obj.blob
 
-    def ids(self) -> Generator[interfaces.Identifier, None, None]:
+    def ids(
+        self,
+    ) -> collections.abc.Generator[interfaces.Identifier, None, None]:
         return (key for key in self._lookup)
 
     def __contains__(self, item: interfaces.T_data) -> bool:
@@ -136,7 +147,7 @@ class ObjectLibraryKeyVal(interfaces.ObjectLibrary, Generic[interfaces.T_data]):
     def __getitem__(self, item: interfaces.Identifier) -> interfaces.T_data:
         return self._initializer(self._lookup[item])
 
-    def __iter__(self) -> Iterator[interfaces.T_data]:
+    def __iter__(self) -> collections.abc.Iterator[interfaces.T_data]:
         return (self._initializer(value) for value in self._lookup.values())
 
     def __len__(self) -> int:
