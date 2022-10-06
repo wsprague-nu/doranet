73bd9fa5f3d7263aa74f57f9ad0a9767a61bf557
wsprague-nu <williamsprague2025@u.northwestern.edu>
2022-10-06 17:10:29 -0500

    added a testing script

---
+++ samples/osmium_testo.py
@@ -0,0 +1,11 @@
+import pickaxe_generic as pg
+
+engine = pg.create_engine()
+
+ozone = engine.mol.rdkit("[O-][O+]=O")
+alkyne = engine.mol.rdkit("CCC#C")
+fluorine = engine.mol.rdkit("F")
+
+op = engine.op.rdkit("[C&+0:1]#[C&+0:2].[F&H1&+0:3].[F&H1&+0:4]>>[*:1]-[*:2](-[*:3])-[*:4]")
+
+print(op(alkyne, fluorine, fluorine))
