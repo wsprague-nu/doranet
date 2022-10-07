212f0522d6686162b3a413db9bfb30b6cdd17ee2
wsprague-nu <williamsprague2025@u.northwestern.edu>
2022-10-07 17:36:45 -0500

    updated test script

--- samples/osmium_testo.py
+++ samples/osmium_testo.py
@@ -6,6 +6,8 @@ ozone = engine.mol.rdkit("[O-][O+]=O")
 alkyne = engine.mol.rdkit("CCC#C")
 fluorine = engine.mol.rdkit("F")
 
-op = engine.op.rdkit("[C&+0:1]#[C&+0:2].[F&H1&+0:3].[F&H1&+0:4]>>[*:1]-[*:2](-[*:3])-[*:4]")
+op = engine.op.rdkit(
+    "[C&+0:1]#[C&+0:2].[F&H1&+0:3].[F&H1&+0:4]>>[*:1]-[*:2](-[*:3])-[*:4]"
+)
 
 print(op(alkyne, fluorine, fluorine))
