43de9c6e70d1cc91141afbc8c6bd76b950b9d6f0
William Sprague <wsprague@u.northwestern.edu>
2024-04-19 15:31:39 -0500

    edited import test

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,9 +1,5 @@
 import pytest
 
-def test_namespace_alias():
+def test_import_package():
     with pytest.raises(ImportError):
-        from doranet import dn
-
-def test_namespace_nesting():
-    with pytest.raises(ImportError):
-        from doranet import doranet
+        import doranet as dn
