014ce918be47901d484b1198ea1caf420bbe892e
William Sprague <wsprague@u.northwestern.edu>
2024-05-24 13:25:25 -0500

    added lines to __all__

--- doranet/modules/post_processing/__init__.py
+++ doranet/modules/post_processing/__init__.py
@@ -1,6 +1,11 @@
 """DORAnet module for post-processing."""
 
-__all__ = ["post_processing"]
+__all__ = [
+    "pathway_finder",
+    "pathway_ranking",
+    "pathway_visualization",
+    "pretreat_networks",
+]
 
 from .post_processing import pretreat_networks
 from .post_processing import pathway_finder
