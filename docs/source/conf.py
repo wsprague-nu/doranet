88d5d60b300ce299305d49c446a01c9ce328f8af
William Sprague <wsprague@u.northwestern.edu>
2024-06-20 12:12:06 -0500

    added type annotation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ myst_fence_as_directive = ["mermaid"]
 numpydoc_show_class_members = False
 
 templates_path = ["_templates"]
-exclude_patterns = []
+exclude_patterns: list[str] = []
 
 source_suffix = {
     ".rst": "restructuredtext",
