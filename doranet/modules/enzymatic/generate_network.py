b7807dfd53b02c28158e3b84b75b00df48bf095f
William Sprague <wsprague@u.northwestern.edu>
2026-01-28 16:11:29 -0600

    fix nan checks

--- doranet/modules/enzymatic/generate_network.py
+++ doranet/modules/enzymatic/generate_network.py
@@ -2,6 +2,7 @@
 
 import collections.abc
 import dataclasses
+import math
 import re
 import time
 import typing
@@ -350,7 +351,7 @@ class Rxn_dH_Filter(metadata.ReactionFilterBase):
         dH = recipe.reaction_meta[self.dH_key]
         if dH == "No_Thermo":
             return True
-        if dH == float("nan"):
+        if math.isnan(dH):
             return False
         return dH < self.max_dH
 
--- doranet/modules/post_processing/post_processing.py
+++ doranet/modules/post_processing/post_processing.py
@@ -1152,7 +1152,7 @@ class Rxn_dH_Filter(metadata.ReactionFilterBase):
         dH = recipe.reaction_meta[self.dH_key]
         if dH == "No_Thermo":
             return True
-        if dH == float("nan"):
+        if math.isnan(dH):
             return False
         return dH < self.max_dH
 
